ac64c2f81b678a0909a4e3b8932f373ff406e660
zigahertz <zach.zibrat@locusenergy.com>
2016-04-05 07:52:22 -0700

    code cleanup

--- addon/helpers/interpolate.js
+++ addon/helpers/interpolate.js
@@ -3,9 +3,7 @@ import Ember from 'ember';
 const matcher = /\$\{\s*(.*?)\s*\}/g;
 
 export function interpolate([ str ], hash) {
-  return str.replace(matcher,  (i, match) => {
-    return Ember.get(hash, match)
-  })
+  return str.replace(matcher,  (i, match) => Ember.get(hash, match));
 }
 
 export default Ember.Helper.helper(interpolate);
